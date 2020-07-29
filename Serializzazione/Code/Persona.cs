65b6217fa1d181de9732056d80e3b3c857a2d33e
Valerio Valdes <valerio.valdes89@outlook.com>
2020-07-29 13:48:50 +0200

    Esempi vari di serializzazione

--- Serializzazione/Code/Persona.cs
+++ Serializzazione/Code/Persona.cs
@@ -1,16 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace Serializzazione.Code
 {
-    [Serializable]
-    class Persona
+    [DataContract]
+    public class Persona
     {
+        public Persona(string nome)
+        {
+            Nome = nome;
+        }
+        [DataMember]
         public string Nome { get; set; }
+        [DataMember]
         public string Cognome { get; set; }
+
         public int Età { get; set; }
+
+        public Automobile Auto { get; set; }
+
+        public string GetAnnoDiNascita()
+        {
+            return DateTime.Now.AddYears(-Età).Year.ToString();
+        }
     }
+
+
+    public class Automobile
+    {
+
+        public string Marca { get; set; }
+
+        public int Cilindrata { get; set; }
+    }
+
+
+    //public class Studente : Persona
+    //{
+    //    public int Matricola { get; set; }
+    //}
 }
--- Serializzazione/Form1.cs
+++ Serializzazione/Form1.cs
@@ -1,4 +1,5 @@
-﻿using Serializzazione.Code;
+﻿using Newtonsoft.Json;
+using Serializzazione.Code;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -6,10 +7,13 @@ using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Serialization;
 
 namespace Serializzazione
 {
@@ -23,13 +27,30 @@ namespace Serializzazione
         private void btn_serialize_Click(object sender, EventArgs e)
         {
             Persona p = GetPersona();
-            FileStream fs = new FileStream("C:\\Corso c#\\Serializzazione.txt", FileMode.Create);
-            BinaryFormatter bf = new BinaryFormatter();
-            bf.Serialize(fs, p);
-            fs.Close();
+            p.Auto = new Automobile { Cilindrata = 1500, Marca = "Fiat" };
+            //Studente s = new Studente() { Matricola = 256 };
+            //SerializzaInXML(p);
+            SerializzaInXMLConDataContract(p);
+            //SerializzaInBinario(p);
+            SerializzaInJSON(p);
+            SerializzaInJsonConNewtonsoft(p);
+
+        }
+            
+        private void btn_deserialize_Click(object sender, EventArgs e)
+        {
+            DeserializzaDaJsonConNewtonsoft();
         }
 
-        private void btn_deserialize_Click(object sender, EventArgs e)
+        public void DeserializzaDaJsonConNewtonsoft()
+        {
+            using (var streamReader = new StreamReader("C:\\Corso c#\\serializzatoJsonConNewtonsoft.json"))
+            {
+                var persona = JsonConvert.DeserializeObject<Persona>(streamReader.ReadLine());
+                ShowPersona(persona);
+            }
+        }
+        public void DeserializzaDaBinario()
         {
             FileStream fs = new FileStream("C:\\Corso c#\\Serializzazione.txt", FileMode.Open);
             var bf = new BinaryFormatter();
@@ -38,7 +59,45 @@ namespace Serializzazione
             ShowPersona(p);
             fs.Close();
         }
+        private void SerializzaInBinario(Persona pippo)
+        {
+            FileStream fs = new FileStream("C:\\Corso c#\\Serializzazione.txt", FileMode.Create);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(fs, pippo);
+            fs.Close();
+        }
+        private void SerializzaInXMLConDataContract(Persona p)
+        {
+            var dcs = new DataContractSerializer(typeof(Persona));
+            FileStream fs = File.OpenWrite("C:\\Corso c#\\serializzatoXmlConDataContract.xml");
+            dcs.WriteObject(fs, p);
+            fs.Close();
+        }
+        private void SerializzaInXML(Persona p)
+        {
+            XmlSerializer dcs = new XmlSerializer(typeof(Persona));
+            FileStream fs = File.OpenWrite("C:\\Corso c#\\serializzato2.xml");
+            dcs.Serialize(fs, p);
+            fs.Close();
+        }
+        private void SerializzaInJSON(Persona p)
+        {
+            var dcs = new DataContractJsonSerializer(typeof(Persona));
+            FileStream fs = File.OpenWrite("C:\\Corso c#\\serializzato.json");
+            dcs.WriteObject(fs, p);
+            fs.Flush();
+            fs.Close();
 
+            
+        }
+
+        private void SerializzaInJsonConNewtonsoft(Persona p)
+        {
+            var sw = new StreamWriter("C:\\Corso c#\\serializzatoJsonConNewtonsoft.json");
+            var personaSerialized = JsonConvert.SerializeObject(p);
+            sw.WriteLine(personaSerialized);
+            sw.Close();
+        }
         private void ShowPersona(Persona p)
         {
             tbx_cognome.Text = p.Cognome;
@@ -48,10 +107,9 @@ namespace Serializzazione
 
         private Persona GetPersona()
         {
-            return new Persona
+            return new Persona(tbx_nome.Text)
             {
-                Cognome = tbx_nome.Text,
-                Nome = tbx_cognome.Text,
+                Cognome = tbx_cognome.Text,
                 Età = int.Parse(tbx_età.Text)
             };
         }
